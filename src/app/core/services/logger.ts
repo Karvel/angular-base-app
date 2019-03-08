3563cbf909bcb095fcfa3d5a511b0bd1a949c2c3
Elanna Grossman <elanna.grossman@gmail.com>
2019-03-07 21:41:52 -0800

    feat(logger): add `Logger`

---
+++ src/app/core/services/logger.ts
@@ -0,0 +1,16 @@
+export class Logger {
+  static error(message: string): void {
+    console.error(message);
+  }
+  static info(message: string): void {
+    console.info(message);
+  }
+
+  static log(message: string): void {
+    console.log(message);
+  }
+
+  static warn(message: string): void {
+    console.warn(message);
+  }
+}
