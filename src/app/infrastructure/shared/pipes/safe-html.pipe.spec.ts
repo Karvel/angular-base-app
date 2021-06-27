058b3a05b4a06b5518207da1513a18d447e3886f
Elanna Grossman <elanna.grossman@gmail.com>
2021-06-27 15:03:44 -0700

    refactor(pipes): remove unused import

--- src/app/infrastructure/shared/pipes/safe-html.pipe.spec.ts
+++ src/app/infrastructure/shared/pipes/safe-html.pipe.spec.ts
@@ -2,7 +2,7 @@ import { SecurityContext } from '@angular/core';
 import { inject, TestBed } from '@angular/core/testing';
 import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 
-import { SpectatorPipe, createPipeFactory, SpyObject } from '@ngneat/spectator';
+import { SpectatorPipe, createPipeFactory } from '@ngneat/spectator';
 
 import { SafeHTMLPipe } from './safe-html.pipe';
 describe('[Unit] SafeHTMLPipe', () => {
