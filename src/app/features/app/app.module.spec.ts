67896f7c3a9ea664d2ed594f65bd1a13dc76fa17
Elanna Grossman <elanna.grossman@gmail.com>
2021-06-27 16:05:56 -0700

    test(app): add smoke test for `AppModule`

---
+++ src/app/features/app/app.module.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { AppModule } from './app.module';
+
+describe('AppModule', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AppModule],
+    });
+  });
+
+  it('initializes', () => {
+    const module = TestBed.inject(AppModule);
+    expect(module).toBeTruthy();
+  });
+});
