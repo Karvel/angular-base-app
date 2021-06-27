72b10354ac6ac0fac426c30832c0847c493f8cc7
Elanna Grossman <elanna.grossman@gmail.com>
2021-06-26 22:45:36 -0700

    feat, test(utils): add `getCopyrightYear()`

--- src/app/infrastructure/utils/utils.ts
+++ src/app/infrastructure/utils/utils.ts
@@ -1,4 +1,15 @@
+import { CONSTANTS } from './constants';
+
 export class Utils {
+  /**
+   * Displays the original copyright year or the range from that year to the current year.
+   */
+  static getCopyrightYear(): string {
+    return CONSTANTS.initialCopyrightYear >= new Date().getFullYear()
+      ? `${new Date().getFullYear()}`
+      : `${CONSTANTS.initialCopyrightYear} - ${new Date().getFullYear()}`;
+  }
+
   public static isMobileDevice(): boolean {
     return (
       typeof window.orientation !== 'undefined' ||
