b9061726aaf6f76d33a47285245277bff98ae3d1
Elanna Grossman <elanna.grossman@gmail.com>
2021-06-27 15:09:21 -0700

    docs(pipes): add jsdoc to `SafeHTMLPipe`

--- src/app/infrastructure/shared/pipes/safe-html.pipe.ts
+++ src/app/infrastructure/shared/pipes/safe-html.pipe.ts
@@ -1,6 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
+/**
+ * Applies the `DomSanitizer` to the passed in template value.
+ */
 @Pipe({ name: 'safeHTML' })
 export class SafeHTMLPipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
