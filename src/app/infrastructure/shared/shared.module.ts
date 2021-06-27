adb074a599905fc00dc717e4651e4145c74ec539
Elanna Grossman <elanna.grossman@gmail.com>
2021-06-26 23:05:24 -0700

    feat, test(app): add `FooterComponent` (#35)
    * feat(constants): add `initialCopyrightYear` constant value
    * feat, test(utils): add `getCopyrightYear()`
    * feat(footer): add `FooterComponent`
    * feat(shared): wire up components import to `SharedModule`
    * feat, test(app): bind footer, add test dependency
    * feat(styles): add global positioning
    * feat(app): add positioning styling
    * refactor(constants): set property as readonly
    * fix(constants): revert readonly status

--- src/app/infrastructure/shared/shared.module.ts
+++ src/app/infrastructure/shared/shared.module.ts
@@ -4,12 +4,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { PipeModule } from './pipes/pipe.module';
 
+import { components } from './components';
+
 /**
  * Connects modules, pipes, directives, components, and modules that do not need to be singleton instances.
  */
 @NgModule({
+  declarations: [components],
   imports: [CommonModule, PipeModule.forRoot(), ReactiveFormsModule],
-  exports: [CommonModule, ReactiveFormsModule],
+  exports: [CommonModule, ReactiveFormsModule, components],
 })
 export class SharedModule {
   static forRoot(): { ngModule: typeof SharedModule } {
