32a4132b4bff6ab7e56513f9de82ce46d63e1bc9
Elanna Grossman <elanna.grossman@gmail.com>
2021-06-27 16:47:31 -0700

    feat, test(pipes): add and wire up `TrackByPipe`

---
+++ src/app/infrastructure/shared/pipes/track-by.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+import { Logger } from '../../utils/logger';
+
+@Pipe({ name: 'trackBy' })
+export class TrackByPipe implements PipeTransform {
+  public transform(key: string): <T, U>(index: number, value: T) => U | null {
+    return function trackByKey<T, U>(index: number, value: T): U | null {
+      if (value[key] === undefined) {
+        Logger.warn(`Key '${key}' not found in ${value}.`);
+      }
+
+      return value ? (value[key] as U) : null;
+    };
+  }
+}
