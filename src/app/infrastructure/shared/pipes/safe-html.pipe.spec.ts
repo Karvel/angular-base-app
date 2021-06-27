67dacaaca3643fb61bc661f9872bb1d4d7a3bd6c
Elanna Grossman <elanna.grossman@gmail.com>
2021-06-26 23:36:20 -0700

    test(pipes): add `SafeHTMLPipe` test

--- src/app/infrastructure/shared/pipes/safe-html.pipe.spec.ts
+++ src/app/infrastructure/shared/pipes/safe-html.pipe.spec.ts
@@ -1,16 +1,31 @@
-import { SafeHTMLPipe } from './safe-html.pipe';
+import { SecurityContext } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
 import { SpectatorPipe, createPipeFactory } from '@ngneat/spectator';
 
+import { SafeHTMLPipe } from './safe-html.pipe';
 describe('[Unit] SafeHTMLPipe', () => {
   let spectator: SpectatorPipe<SafeHTMLPipe>;
   const createPipe = createPipeFactory(SafeHTMLPipe);
 
-  it('should return sanitized input', () => {
-    const expectedValue: string = `Cross`;
-    spectator = createPipe(
-      `{{ Cross <script>alert('Hello')</script> | safeHTML }}`,
+  it(`should return sanitized input`, () => {
+    const sanitizer = spectator?.inject(DomSanitizer);
+    const pipe = new SafeHTMLPipe(sanitizer);
+    const safeHTML = pipe.transform(`Cross <script>alert('Hello')</script>`);
+    const sanitizedValue = sanitizer.sanitize(
+      SecurityContext.RESOURCE_URL,
+      safeHTML,
     );
+    const expectedValue: string = `Cross <script>alert('Hello')</script>`;
 
-    expect(spectator.element).toHaveText(expectedValue);
-  });
+    expect(sanitizedValue).toBe(expectedValue);
+  }),
+    it('should return sanitized input spectator', () => {
+      const expectedValue: string = `Cross`;
+      spectator = createPipe(
+        `{{ Cross <script>alert('Hello')</script> | safeHTML }}`,
+      );
+
+      expect(spectator.element).toHaveText(expectedValue);
+    });
 });
