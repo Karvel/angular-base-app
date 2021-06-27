fc13817df27b15a1aa2e4adaf5882a772ae9bab0
Elanna Grossman <elanna.grossman@gmail.com>
2021-06-27 15:07:23 -0700

    feat, test(pipes): add `FormErrorPipe` and tests, wire up

---
+++ src/app/infrastructure/shared/pipes/form-error.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+/**
+ * Returns the first error value in the `AbstractControl` error object.
+ * Expects an object with string or boolean key-value pairs.
+ */
+@Pipe({ name: 'formError' })
+export class FormErrorPipe implements PipeTransform {
+  public transform(value: string | object): string {
+    if (!value) {
+      return '';
+    }
+    if (typeof value !== 'object') {
+      return value;
+    }
+    const key: string = value[Object.keys(value)[0]];
+
+    return key;
+  }
+}
--- src/app/infrastructure/shared/pipes/index.ts
+++ src/app/infrastructure/shared/pipes/index.ts
@@ -1,6 +1,7 @@
+import { FormErrorPipe } from './form-error.pipe';
 import { SafeHTMLPipe } from './safe-html.pipe';
 
 /**
  * Add pipes that do not need to be specifically referenced.
  */
-export const pipes = [SafeHTMLPipe];
+export const pipes = [FormErrorPipe, SafeHTMLPipe];
