7ad42c10807197a8842418f59bb0146aee8bf335
Elanna Grossman <elanna.grossman@gmail.com>
2021-06-26 22:44:14 -0700

    feat(constants): add `initialCopyrightYear` constant value

--- src/app/infrastructure/utils/constants.ts
+++ src/app/infrastructure/utils/constants.ts
@@ -1,6 +1,7 @@
 interface Constants {
   readonly DIGIT_REGEX: RegExp;
   readonly EMAIL_REGEX: RegExp;
+  initialCopyrightYear: number;
   readonly SYMBOL_REGEX: RegExp;
 }
 
@@ -8,5 +9,6 @@ export const CONSTANTS: Constants = {
   DIGIT_REGEX: /[0-9]/,
   EMAIL_REGEX:
     /^[a-z0-9!#$%&'*+\/=?^_\`{|}~.-]+@[a-z0-9]([a-z0-9-])+(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i,
+  initialCopyrightYear: 2021,
   SYMBOL_REGEX: /[-+_!@#$%^&*,.?]/,
 };
