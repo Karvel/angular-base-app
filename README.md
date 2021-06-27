8c77ad5835b22a8330da5d123ae3a6eac67a4fe4
Elanna Grossman <elanna.grossman@gmail.com>
2021-06-26 18:50:13 -0700

    chore, docs, refactor(app): update to angular 12 (#20)
    * chore(app): upgrade to Angular 8
    * chore(app): upgrade to Angular 9
    * chore(app): upgrade to Angular 10
    * chore(app): upgrade to Angular 11
    * chore(app): upgrade to Angular 12
    * chore(package, yarn): update `ts-node` dependency
    * docs(README): indicate current Angular version
    * chore(browserlistrc): update support browser list
    * chore(app): update angular.json
    * chore(gitignore): update ignore
    * chore(app): update karma coverage dependencies
    * chore(karma): update coverage config
    * docs(package): add author info
    * chore(package, yarn): remove dead dependencies

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AngularBaseApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.3 and has been updated to version 12.1.0.
 
 ## Development server
 
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +6,4 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent implements OnInit {}
+export class HomeComponent {}
