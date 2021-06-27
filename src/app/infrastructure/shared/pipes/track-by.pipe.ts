f9032ecedd523d10a4b9e4bcfae67038b3dcc7c9
Elanna Grossman <elanna.grossman@gmail.com>
2021-06-27 16:54:48 -0700

    refactor(track-by-pipe): improve warning message

--- src/app/infrastructure/shared/pipes/track-by.pipe.ts
+++ src/app/infrastructure/shared/pipes/track-by.pipe.ts
@@ -7,7 +7,7 @@ export class TrackByPipe implements PipeTransform {
   public transform(key: string): <T, U>(index: number, value: T) => U | null {
     return function trackByKey<T, U>(index: number, value: T): U | null {
       if (value[key] === undefined) {
-        Logger.warn(`Key '${key}' not found in ${value}.`);
+        Logger.warn(`Key '${key}' not found in *ngFor.`);
       }
 
       return value ? (value[key] as U) : null;
