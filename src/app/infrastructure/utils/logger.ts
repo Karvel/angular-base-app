61b1856e5d31b8e58a5d4f20803fe8bd8ce0ad2a
Elanna Grossman <elanna.grossman@gmail.com>
2021-06-27 17:02:25 -0700

    feat, test(pipes): add `TrackByPipe` (#49)
    * test(logger): ignore methods for coverage
    * feat, test(pipes): add and wire up `TrackByPipe`
    * refactor(track-by-pipe): improve warning message
    * test(pipes): use correct spy value

--- src/app/infrastructure/utils/logger.ts
+++ src/app/infrastructure/utils/logger.ts
@@ -1,16 +1,21 @@
 /* eslint-disable no-console */
 export class Logger {
+  /* istanbul ignore next */
   static error(message: string): void {
     console.error(message);
   }
+
+  /* istanbul ignore next */
   static info(message: string): void {
     console.info(message);
   }
 
+  /* istanbul ignore next */
   static log(message: string): void {
     console.log(message);
   }
 
+  /* istanbul ignore next */
   static warn(message: string): void {
     console.warn(message);
   }
