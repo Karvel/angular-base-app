9b819e6936e185bede7adc5b9cfb454406b9f21c
Elanna Grossman <elanna.grossman@gmail.com>
2021-06-27 17:44:42 -0700

    chore(karma): add `lcovonly` report for Codecov

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,11 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/angular-base-app'),
       subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+      reporters: [
+        { type: 'html' },
+        { type: 'lcovonly' },
+        { type: 'text-summary' },
+      ],
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
