b3b99190a241e9f7786fd55d262024afdffdf77d
Elanna Grossman <elanna.grossman@gmail.com>
2021-06-27 20:49:47 -0700

    fix(form): constrain generic parameter

--- src/app/infrastructure/core/services/form.service.ts
+++ src/app/infrastructure/core/services/form.service.ts
@@ -9,7 +9,10 @@ export class FormService {
    * Build a request payload from the matching `FormGroup` values.
    * Does not handle conditional logic.
    */
-  public buildRequestPayload<T>(form: FormGroup, requestPayload: T): T {
+  public buildRequestPayload<T extends Object>(
+    form: FormGroup,
+    requestPayload: T,
+  ): T {
     for (const property in requestPayload) {
       if (
         requestPayload.hasOwnProperty(property) &&
