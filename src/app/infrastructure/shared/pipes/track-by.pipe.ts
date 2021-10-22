6071a9bb9347726d113bf7254afc1e15c4dd2d2b
Elanna Grossman <elanna.grossman@gmail.com>
2021-10-21 20:47:04 -0700

    refactor(track-by-pipe): remove dead code

--- src/app/infrastructure/shared/pipes/track-by.pipe.ts
+++ src/app/infrastructure/shared/pipes/track-by.pipe.ts
@@ -4,7 +4,7 @@ import { Logger } from '../../utils/logger';
 
 @Pipe({ name: 'trackBy' })
 export class TrackByPipe implements PipeTransform {
-  public transform<T>(key: string): (index: number, value: any) => any | null {
+  public transform(key: string): (index: number, value: any) => any | null {
     return function trackByKey(index: number, value: any): any | null {
       if (value[key] === undefined) {
         Logger.warn(`Key '${key}' not found in *ngFor.`);
